a285f868ee5a3a7da7b51c75e620236518d7a80c
aashish.biradar <aashish.biradar@cimpress.com>
2022-11-24 03:59:00 +0000

    Add (backbone): ping api

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 
 const router = Router();
 
+router.get('/ping', (_, res) => res.send('pong'));
 router.use('/user', require('./user'));
 
 module.exports = router;
