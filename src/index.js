a59acefbba34fa1d4b58d957e1c0612a85c8bc88
aashish.biradar <aashish.biradar@cimpress.com>
2022-11-28 06:36:09 +0000

    Update(backbone):Package Update and  Cors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const config = require('../config');
 const routes = require('./routes/routes');
 const swagger = require('./services/swagger');
+const cors = require('cors');
 const errorHandler = require('./services/errorHandler');
 
 const { port } = config;
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use('/api', routes);
 app.use('/docs', swagger.serve, swagger.setup());
 app.use(errorHandler);
